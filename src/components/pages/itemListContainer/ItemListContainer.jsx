7f556db4b1ce1fd246536be75944ec6c0c8552d5
Facundo Labriola <facundolabriola@hotmail.com>
2023-11-09 11:12:38 -0300

    por las dudas

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -3,11 +3,19 @@ import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
 import { Box, Button } from "@mui/material";
 import CardSkeleton from "../../common/cardSkeleton/CardSkeleton";
-import { getDocs, collection, query, where } from "firebase/firestore";
+import { getDocs, collection, query, where, addDoc } from "firebase/firestore";
 import { db } from "../../../firebaseConfig";
+import { products } from "../../../productsMock";
+
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
   const { categoryName } = useParams();
+  // const rellenarDB = () => {
+  //   const productCollection = collection(db, "products");
+  //   products.forEach((elemento) => {
+  //     addDoc(productCollection, elemento);
+  //   });
+  // };
 
   useEffect(() => {
     let productCollection = collection(db, "products");
@@ -30,9 +38,9 @@ const ItemListContainer = () => {
 
   return (
     <>
-      <Button variant="contained" color="secondary">
+      {/* <Button onClick={rellenarDB} variant="contained" color="secondary">
         Rellenar
-      </Button>
+      </Button> */}
       {items.length === 0 ? (
         <Box style={{ display: "flex", gap: 20 }}>
           <CardSkeleton />
